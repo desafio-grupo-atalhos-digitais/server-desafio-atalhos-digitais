3b22913d0d4e8e7ea5641922f5f2af615e46e426
José Henrique <josehenrique.contato.dev@gmail.com>
2026-08-19 15:02:18 -0300

    fix: remove unused WebhookControllerController import from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './services/user/user.module';
-import { WebhookControllerController } from './controllers/webhook-controller/webhook-controller.controller';
 
 @Module({
   imports: [UserModule],
-  controllers: [AppController, WebhookControllerController],
+  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
